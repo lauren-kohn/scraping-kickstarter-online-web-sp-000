888965bc21f4f6b919ec663a2cad2ece97258806
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-10 23:02:59 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,6 +4,12 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("li.project.grid_4")
+      projects[title.to_sym] = {}
   #projects: kickstarter.css("li.project.grid_4")
   #title: project.css("h2.bbcard_name strong a").text
   #image link: project.css("div.project-thumbnail a img").attribute("src").value
